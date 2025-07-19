07aed73b843ca2e3c1bc11737b9e3f3c3cf9e151
David Y. Stephenson <david@davidystephenson.com>
2025-07-19 12:32:05 -0400

    Populate select

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ function initialise () {
     const totalIncome = document.getElementById('total-income')
     const transactionsList = document.getElementById('transactions')
     const balanceSection = document.getElementById('balance')
+    const expenseButton = document.getElementById('add-transaction-btn')
+    const descriptionInput = document.getElementById('description')
+    const expenseInput = document.getElementById('amount')
+    const categorySelect = document.getElementById('category')
+    for (category in categoryWiseBudget) {
+        categorySelect.innerHTML += `<option value='${category}'>${category}</option>`
+    }
     incomeForm.addEventListener('submit', (event) => {
         event.preventDefault()
         const value = Number(incomeInput.value)
@@ -35,5 +42,12 @@ function initialise () {
 
         const balance = income - expense
         balanceSection.innerHTML = `<b>Balance: $${balance}`
+    })
+    expenseButton.addEventListener('click', () => {
+        const value = Number(expenseInput.value)
+        expense += value
+        transactionsList.innerHTML = `<li>
+            <span class='category'>
+        </li>`
     })
 }
